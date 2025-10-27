91953385578de58b8d89486eb1637dbad892ae15
Antonio <tonikis21@gmail.com>
2025-10-27 17:48:45 +0100

    refactor(utils): change utility function to save and show figure when called

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -9,15 +9,23 @@ from matplotlib import pyplot as plt
 from src.utils.types import Rectangle
 
 
-def draw_solution(img: np.ndarray, chromosomes: List[Rectangle]):
+def draw_solution(
+    img: np.ndarray,
+    chromosomes: List[Rectangle],
+    save_path: str = None,
+    show: bool = False
+):
     """Draw binary image with colored rectangle overlay.
 
     Args:
         img: Binary image.
         chromosomes: List of Chromosome objects or rectangles.
+        save_path: Path to save figure (optional).
+        show: Whether to display figure (default: True).
 
     """
     height, width = img.shape
+    plt.figure(figsize=(10, 10))
     plt.imshow(img, cmap="gray", origin="upper", extent=[0, width, height, 0])
 
     # Get rectangles from Chromosome object or list
@@ -32,4 +40,12 @@ def draw_solution(img: np.ndarray, chromosomes: List[Rectangle]):
             )
         )
     plt.axis("off")
-    plt.show()
+    plt.title(f"Solution: {len(rects)} rectangles")
+
+    if save_path:
+        plt.savefig(save_path, bbox_inches='tight', dpi=150)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
